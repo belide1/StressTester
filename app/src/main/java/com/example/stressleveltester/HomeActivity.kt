3708260c2d141f50f5b0c24961c163d7279ea3c1
belide1 <belidesravan009@gmail.com>
2025-04-04 23:50:43 +0530

    Implemented the HomeActivity with a bottom navigation bar with option StartTest, Graph Analysis and MyProfile Section.

---
+++ app/src/main/java/com/example/stressleveltester/HomeActivity.kt
@@ -0,0 +1,14 @@
+package com.example.stressleveltester
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class HomeActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
